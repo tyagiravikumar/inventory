7ff2e8b3128bfa0ba295049d769563915919c44b
Ravi Tyagi <tyagiravikumar@gmail.com>
2017-09-15 01:06:27 -0400

    update class

--- src/app/invoice/invoice.ts
+++ src/app/invoice/invoice.ts
@@ -6,20 +6,8 @@ export class Invoice{
     address ?: string;
     product ?: Array<Product> = [];
     discount ?:number;
-    
+    cgst?:number;
+    sgst?:number;
     date ?:number;
-    
-    get cgst():number {
-        if(this.product.length>0) return this.product[0].cgst;
-        else return 0;
-    }
-    get sgst():number {
-        if(this.product.length>0) return this.product[0].sgst;
-        else return 0;
-    }
-    get Total():number{
-          return 0;  
-    }
-
 
 }
